57fd319ae690e29d68e633355a7821df6c9d97d9
jorge dos santos <you@example.com>
2021-06-03 12:28:48 -0300

    atualizando

--- curso js/aula12ex/exercicio14/script.js
+++ curso js/aula12ex/exercicio14/script.js
@@ -3,9 +3,9 @@ function carregar() {
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
     var data = new Date()
-    var hora = data.getHours()
+    //var hora = data.getHours()
     
-    
+    var hora = 19
     msg.innerHTML = `agora são ${hora} horas.`
     if(hora >= 0 && hora < 12 ){
         //BOM DIA!
